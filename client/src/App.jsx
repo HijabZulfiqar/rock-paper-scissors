c90b876d32ade55cd83818ce0d74cd6e9de4c0e3
Hijab Zulfiqar <hijabzulfiqar2001@gmail.com>
2024-07-26 13:25:57 +0500

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,3 @@
-function App() {
-  return (
-    <>
-      <h1 className="font-bold text-center">rock paper scissors</h1>
-    </>
-  );
+export default function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
-
-export default App;
